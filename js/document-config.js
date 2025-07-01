2d86933434c1970a320d291127b9cd9d89ae463e
Monique van Scherpenzeel <m.vanscherpenzeel@geonovum.nl>
2025-07-01 14:48:58 +0200

    update werkversie
    naar versie 2.0 en nieuwe structuur

---
+++ js/document-config.js
@@ -0,0 +1,44 @@
+var documentConfig =
+{
+  title: "Wijzigingsprotocol geo-standaarden",
+  shortName: "WijzigingsprotocolGeoStandaarden",
+  pubDomain: "geen",
+  specStatus: "wv",
+  specType: "bd",
+  license: "cc-by-nd",
+  latestVersion: [
+    "https://docs.geostandaarden.nl/geen/WijzigingsprotocolGeoStandaarden/"
+  ],
+  publishDate: "2025-07-01",
+  thisVersion: [
+    "https://geonovum.github.io/geen-WijzigingsprotocolGeoStandaarden/"
+  ],
+  previousPublishDate: "2021-10-19",
+  prevVersion: [
+    "https://docs.geostandaarden.nl/geen/def-bd-WijzigingsprotocolGeoStandaarden-20211019/"
+  ],
+  previousMaturity: "def",
+  github: "https://github.com/geonovum/geen-WijzigingsprotocolGeoStandaarden/",
+  issueBase: "https://github.com/geonovum/geen-WijzigingsprotocolGeoStandaarden/issues/",
+  edDraftURI: "https://geonovum.github.io/geen-WijzigingsprotocolGeoStandaarden/",
+  canonicalURI: "https://docs.geostandaarden.nl/geen/WijzigingsprotocolGeoStandaarden/",
+  authors: [
+    {
+      name: "Monique van Scherpenzeel",
+      company: "Geonovum",
+      companyURL: "https://www.geonovum.nl/"
+    }
+  ],
+  editors: [
+    {
+      name: "Dick Krijtenburg",
+      company: "Geonovum",
+      companyURL: "https://www.geonovum.nl/"
+    },
+    {
+      name: "Monique van Scherpenzeel",
+      company: "Geonovum",
+      companyURL: "https://www.geonovum.nl/"
+    }
+  ]
+}

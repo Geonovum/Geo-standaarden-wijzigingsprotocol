8104d9a938af16338bfa2d1ae22ee6c68ddb2bee
Monique van Scherpenzeel <m.vanscherpenzeel@geonovum.nl>
2025-07-09 13:59:47 +0200

    update config.js

--- js/document-config.js
+++ js/document-config.js
@@ -1,27 +1,27 @@
 var documentConfig =
 {
   title: "Wijzigingsprotocol geo-standaarden",
-  shortName: "WijzigingsprotocolGeoStandaarden",
-  pubDomain: "geen",
+  shortName: "gsw",
+  pubDomain: "gsw",
   specStatus: "wv",
   specType: "bd",
   license: "cc-by-nd",
   latestVersion: [
-    "https://docs.geostandaarden.nl/geen/WijzigingsprotocolGeoStandaarden/"
+    "https://docs.geostandaarden.nl/gsw/gsw/"
   ],
-  publishDate: "2025-07-01",
+  publishDate: "2025-07-09",
   thisVersion: [
-    "https://geonovum.github.io/geen-WijzigingsprotocolGeoStandaarden/"
+    "https://geonovum.github.io/Geo-standaarden-wijzigingsprotocol/"
   ],
   previousPublishDate: "2021-10-19",
   prevVersion: [
-    "https://docs.geostandaarden.nl/geen/def-bd-WijzigingsprotocolGeoStandaarden-20211019/"
+    "https://docs.geostandaarden.nl/gsw/def-bd-gsw-20211019/"
   ],
   previousMaturity: "def",
-  github: "https://github.com/geonovum/geen-WijzigingsprotocolGeoStandaarden/",
-  issueBase: "https://github.com/geonovum/geen-WijzigingsprotocolGeoStandaarden/issues/",
-  edDraftURI: "https://geonovum.github.io/geen-WijzigingsprotocolGeoStandaarden/",
-  canonicalURI: "https://docs.geostandaarden.nl/geen/WijzigingsprotocolGeoStandaarden/",
+  github: "https://github.com/Geonovum/Geo-standaarden-wijzigingsprotocol",
+  issueBase: "https://github.com/Geonovum/Geo-standaarden-wijzigingsprotocol/issues/",
+  edDraftURI: "https://geonovum.github.io/Geo-standaarden-wijzigingsprotocol/",
+  canonicalURI: "https://docs.geostandaarden.nl/gsw/gsw/",
   authors: [
     {
       name: "Monique van Scherpenzeel",

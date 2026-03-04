e8c55882c0aeb980bade2d7ca5ba90d9d84e2272
Paul Janssen <p.janssen@geonovum.nl>
2026-03-04 11:30:25 +0100

    Update config.js

--- js/config.js
+++ js/config.js
@@ -3,8 +3,8 @@ let respecConfig = {
   useLabel: true,
   title: "Wijzigingsprotocol geo-standaarden",
   //-- specStatus is verplicht! (activeer 1 van de volgende) --------------------------
-  //specStatus: "wv",                   // Werkversie
-  specStatus: "cv",               // Consultatieversie
+  specStatus: "wv",                   // Werkversie
+  //specStatus: "cv",               // Consultatieversie
   //specStatus: "vv",               // Versie ter vaststelling
   //specStatus: "def",              // Vastgestelde versie
   //specStatus: "basis",            // Basis Document
@@ -22,7 +22,7 @@ let respecConfig = {
 
   //-- publishDate is verplicht -------------------------------------------------------
   //-- NB: in de werkversie uitzetten, want dan pakt Respec de pushdate ---------------
-  publishDate: "2025-10-09",
+  //publishDate: "2025-10-09",
   //eventueel is het mogelijk een versienummer mee te geven, maar bij Geonovum werken we gewoonlijk alleen met datum als onderdeel van de permanente URI.
   publishVersion: "2.0.0",
   previousVersion: "1.1.0",
